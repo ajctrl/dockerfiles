3066f4024866a5baaac0ee9f5cb55d235f9d96fc
ajctrl <59609985+ajctrl@users.noreply.github.com>
2022-10-19 21:18:19 +0000

    pg15 compile

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -4,7 +4,19 @@
 set -eo pipefail
 
 apk update
-apk add postgresql-client openssl aws-cli
+apk add openssl aws-cli
+
+wget https://ftp.postgresql.org/pub/source/v15.0/postgresql-15.0.tar.gz
+tar xzf postgresql-15.0.tar.gz
+rm postgresql-15.0.tar.gz
+cd postgresql-15.0
+apk add make gcc libtool automake autoconf g++ readline-dev zlib-dev linux-headers
+./configure --with-pgport=5432 --prefix=/usr/bin/
+make
+make install
+apk delete make gcc libtool automake autoconf g++ readline-dev zlib-dev linux-headers
+cd ../
+rm -rf postgresql-15.0
 
 # cleanup
 rm -rf /var/cache/apk/*
